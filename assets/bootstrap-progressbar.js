93e5515a8e71439889daf5decf43bd2b86ee395c
agilejzl <agilejzl@gmail.com>
2013-10-25 16:47:00 +0800

    修改进度条为单颜色

--- assets/bootstrap-progressbar.js
+++ assets/bootstrap-progressbar.js
@@ -5,9 +5,9 @@
 		this.percent = 0;
 		var hasOptions = typeof options == 'object';
 
-		this.markers = $.fn.progressbar.defaults.markers;
-		if (hasOptions && typeof options.markers == 'object') {
-			this.setMarkers(options.markers);
+		this.step = $.fn.progressbar.defaults.step;
+		if (hasOptions && typeof options.step == 'number') {
+			this.setStep(options.step);
 		}
 
 		this.maximum = $.fn.progressbar.defaults.maximum;
@@ -15,13 +15,15 @@
 			this.setMaximum(options.maximum);
 		}
 
-		this.step = $.fn.progressbar.defaults.step;
-		if (hasOptions && typeof options.step == 'number') {
-			this.setStep(options.step);
+		this.barClass = $.fn.progressbar.defaults.barClass;
+		if (hasOptions && typeof options.barClass == 'string') {
+			this.setBarClass(options.barClass);
 		}
 
-		this.setTemplate(this.markers)
-		this.element.html(this.template);
+		this.markers = $.fn.progressbar.defaults.markers;
+		if (hasOptions && typeof options.markers == 'object') {
+			this.setMarkers(options.markers);
+		}
 	};
 
 	ProgressBar.prototype = {
@@ -35,6 +37,11 @@
 
 		setMarkers: function (markers) {
 			this.markers = markers;
+			this.setTemplate()
+		},
+
+		setBarClass: function (barClass) {
+			this.barClass = barClass;
 		},
 
 		setMaximum: function (maximum) {
@@ -48,18 +55,16 @@
 			this.step = parseInt(step);
 		},
 
-		setTemplate: function (markers) {
-			template = '<div class="progress progress-mini progress-striped active" style="height: 10px;"><span class="state-position badge" style="position: absolute; left: 0%; top: -5px;">0</span>'
-			
-			for(var i=0; i < markers.length; i++) {
-				template += '<div class="bar ' + markers[i].barClass + ' progress-bar progress-' + markers[i].barClass +'" style="width: 0%;left: -1%"></div>';
-				position = 100;
-				if(i < markers.length-1) {
-					position = markers[i+1].position;
-				}
-				template += '<span class="badge ' + markers[i].badgeClass + '" style="position: absolute; left: ' + (position - 1) + '%; top: -5px;">' + (i + 1) + '</span>';
+		setTemplate: function () {
+			template = 
+				'<div class="progress progress-mini progress-striped active" style="height: 10px;">' + 
+					'<span class="state-position badge" style="position: absolute; left: 0%; top: -5px;">0</span>' + 
+						'<div class="bar ' + this.barClass + ' progress-bar progress-' + this.barClass +'" style="width: 0%;left: -1%"></div>';
+
+			for(var i = 0; i < this.markers.length; i++) {
+				template += '<span class="badge ' + this.markers[i].badgeClass + '" style="position: absolute; left: ' + (this.markers[i].position-1) + '%; top: -5px;">' + (i + 1) + '</span>';
 			}
-			return this.template = template + '</div>';
+			this.element.html(template + '</div>')
 		},
 
 		setPosition: function (position) {
@@ -72,34 +77,26 @@
 			this.position = position;
 			this.percent = Math.ceil((this.position / this.maximum) * 100);
 			try {
-				for(var i = 0; i < this.markers.length; i++) {
-					// 使每一个if代码块都只执行一次
-					if(i == 0) {
-						if (this.percent <= this.markers[i+1].position) {
-							for(j in this.markers) {
-								if(j <= i){
-									this.element.find('.' + this.markers[j].barClass).css('width', this.percent + "%");
-								} else {
-									this.element.find('.' + this.markers[j].barClass).css('width', "0%");
-								}
-							}
-							return;
-						}
-					} else if (i > 0 && i < this.markers.length - 1) {
-						// i >= 1 && i <= length-2
-						this.element.find('.' + this.markers[i-1].barClass).css('width', (this.markers[i].position - this.markers[i-1].position) + "%");
-						if (this.percent > this.markers[i].position && this.percent <= this.markers[i+1].position) {
-							this.element.find('.' + this.markers[i].barClass).css('width', (this.percent - this.markers[i].position) + "%");
-							for(var j = i+1; j < this.markers.length; j++){
-								this.element.find('.' + this.markers[j].barClass).css('width', "0%");
-							}
-							return;
-						}
-					} else if (i == this.markers.length - 1) {
-						this.element.find('.' + this.markers[i-1].barClass).css('width', (this.markers[i].position - this.markers[i-1].position) + "%");
-						this.element.find('.' + this.markers[i].barClass).css('width', (this.percent - this.markers[i].position) + "%");
+				current = this.percent - this.step;
+				length = this.markers.length;
+				if (current >= 0 && current < this.markers[0].position) {
+					// 当前进度在step0
+					this.element.find('.' + this.barClass).css('width', this.percent + "%");
+					return;
+				}
+
+				for(var i = 1; i <= length-2; i++) {
+					if (current >= this.markers[i-1].position && current < this.markers[i].position) {
+						// 当前进度在step1 —— step(n-2)
+						this.element.find('.' + this.barClass).css('width', this.percent + "%");
 						return;
 					}
+				}
+
+				if (current >= this.markers[length-2].position && current < this.markers[length-1].position) {
+					// 当前进度在step(n-1)
+					this.element.find('.' + this.barClass).css('width', this.percent + "%");
+					return;
 				}
 			} finally {
 				this._triggerPositionChanged();
@@ -110,9 +107,7 @@
 			this.position = 0;
 			this.percent = 0;
 			this._triggerPositionChanged();
-			for(i in this.markers) {
-				this.element.find("." + this.markers[i].barClass).css('width', "0%");
-			}
+			this.element.find("." + this.barClass).css('width', "0%");
 		},
 
 		_triggerPositionChanged: function () {
@@ -142,14 +137,15 @@
 	};
 
 	$.fn.progressbar.defaults = {
-		markers: 
-			[ { name: "step0", position: 0, barClass: "bar-info" , badgeClass: "badge-info" },
-				{ name: "step1", position: 25, barClass: "bar-success", badgeClass: "badge-success" }, 
-			  { name: "step2", position: 50, barClass: "bar-warning", badgeClass: "badge-warning" }, 
-			  { name: "step3", position: 75, barClass: "bar-danger", badgeClass: "badge-important" }
-			],
+		step: 20,
 		maximum: 100,
-		step: 10
+		barClass: "bar-danger",
+		markers: 
+			[ { position: 25, badgeClass: "badge-info" },
+				{ position: 50, badgeClass: "badge-success" }, 
+				{ position: 75, badgeClass: "badge-warning" }, 
+				{ position: 100, badgeClass: "badge-important" }
+			]
 	};
 
 	$.fn.progressbar.Constructor = ProgressBar;
